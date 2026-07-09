5444b0bc64b6ce9a430a94f9420dbccb74c91b04
Hoang An <hoangan726@gmail.com>
2026-07-08 17:33:21 -0700

    fix: set base path config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/an-nguyen-portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
